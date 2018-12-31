6469c25d67885e86bae6ff0e1b4c93729f672b17
jfaramarz <jfaramarz@gmail.com>
2018-12-31 13:22:39 +0330

    Create SumClass for Sum Operate

---
+++ TestExam/TestExam/SumClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExam
+{
+    class SumClass
+    {
+        public int a = 0 , b = 0 , c = 0;
+        
+        public void summ()
+        {
+            c = a + b;
+        }
+    }
+}
